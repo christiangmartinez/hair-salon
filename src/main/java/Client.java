5689ead6100e8da8e4ea7fd8c8e40991f6374c54
Christian Martinez <cgmartinez11142@gmail.com>
2017-03-31 09:57:02 -0700

    add methods and test to save to database, assign Id, and return all instances of client

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,4 +1,6 @@
 import org.sql2o.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Client {
   private int id;
@@ -37,6 +39,25 @@ public class Client {
       return this.getName().equals(newClient.getName())
         && this.getPhoneNumber().equals(newClient.getPhoneNumber())
         && this.getAddress().equals(newClient.getAddress());
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO clients (name, phoneNumber, address) VALUES (:name, :phoneNumber, :address)";
+      this.id = (int) con.createQuery(sql, true)
+      .addParameter("name", this.name)
+      .addParameter("phoneNumber", this.phoneNumber)
+      .addParameter("address", this.address)
+      .executeUpdate()
+      .getKey();
+    }
+  }
+
+  public static List<Client> all() {
+    String sql = "SELECT id, name, phoneNumber, address FROM clients";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Client.class);
     }
   }
 
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -28,4 +28,29 @@ public class ClientTest {
     assertTrue(firstClient.equals(secondClient));
   }
 
+  @Test
+  public void save_savesClientToDatabase_true() {
+    Client testClient = new Client("Chulo", "555-555-5555", "123 Chulo lane");
+    testClient.save();
+    assertTrue(Client.all().get(0).equals(testClient));
+  }
+
+  @Test
+  public void save_assignsIdToClient() {
+    Client testClient = new Client("Chulo", "555-555-5555", "123 Chulo lane");
+    testClient.save();
+    Client savedClient = Client.all().get(0);
+    assertEquals(testClient.getId(), savedClient.getId());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfClient_true() {
+    Client firstClient = new Client("Chulo", "555-555-5555", "123 Chulo lane");
+    firstClient.save();
+    Client secondClient = new Client("Valentina", "999-999-9999", "123 Chulo lane");
+    secondClient.save();
+    assertTrue(Client.all().get(0).equals(firstClient));
+    assertTrue(Client.all().get(1).equals(secondClient));
+  }
+
 }
